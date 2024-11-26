915f470f803962074cf306e524c5b1d369c2d0f7
Steffen Richters-Finger <steffen.finger.privat@gmail.com>
2024-11-26 21:06:15 +0100

    [backend] add GET-/user/<>-endpoints

--- backend/api/v0.py
+++ backend/api/v0.py
@@ -1,14 +1,24 @@
 """Definition of blueprint for backend API v0."""
 
-from flask import Blueprint, Response
+from pathlib import Path
+
+from flask import Blueprint, Response, jsonify, send_file
+
+from ..common import User  # pylint: disable=relative-beyond-top-level
 
 
-def blueprint_factory() -> Blueprint:
+def blueprint_factory(user_json_file: Path, user: User) -> Blueprint:
     """Returns a flask-Blueprint implementing the API v0."""
     bp = Blueprint("v0", "v0")
 
-    @bp.route("/ping", methods=["GET"])
-    def ping():
-        return Response("pong", mimetype="text/plain", status=200)
+    @bp.route("/user/name", methods=["GET"])
+    def user_name():
+        return Response(user.name, mimetype="text/plain", status=200)
+
+    @bp.route("/user/avatar", methods=["GET"])
+    def user_image():
+        if user.avatar:
+            return send_file(user.avatar)
+        return jsonify(user.avatar), 404
 
     return bp
