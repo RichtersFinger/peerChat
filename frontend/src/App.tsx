5ebaa26aae8592515b9e48c9c183088476b76d74
RichtersFinger <steffen.finger.privat@gmail.com>
2024-11-27 21:13:48 +0100

    [frontend] setup test-ui for API-connection to backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,24 @@
+import { useRef } from "react";
+
 function App() {
-  return <div></div>;
+  const pingRef = useRef<HTMLParagraphElement>(null);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          fetch("http://localhost:5000/ping")
+            .then((response) => response.text())
+            .then((text) => {
+              if (pingRef.current) pingRef.current.innerText = text;
+            });
+        }}
+      >
+        ping
+      </button>
+      <p ref={pingRef}></p>
+    </div>
+  );
 }
 
 export default App;
