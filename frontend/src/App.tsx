cb16380244438fa62e2e21a2cefffb6e42832682
Steffen Richters-Finger <steffen.finger.privat@gmail.com>
2024-11-28 20:35:11 +0100

    [frontend] add test-ui for authentication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,11 +4,15 @@ import { io } from "socket.io-client";
 
 const socket = io("http://localhost:5000", {
   autoConnect: false,
+  withCredentials: true,
 });
 
 export default function App() {
   const pingRef = useRef<HTMLParagraphElement>(null);
   const eventRef = useRef<HTMLParagraphElement>(null);
+  const createKeyInputRef = useRef<HTMLInputElement>(null);
+  const createKeyRef = useRef<HTMLParagraphElement>(null);
+
   socket.on("event-response", (value) => {
     if (eventRef.current) eventRef.current.innerText = JSON.stringify(value);
   });
@@ -27,6 +31,27 @@ export default function App() {
         ping
       </button>
       <p ref={pingRef}></p>
+      <input ref={createKeyInputRef} type="text" />
+      <button
+        onClick={() => {
+          fetch("http://localhost:5000/auth/key", {
+            method: "POST",
+            body: JSON.stringify(
+              createKeyInputRef.current
+                ? { peerChatAuth: createKeyInputRef.current.value }
+                : {}
+            ),
+            headers: { "content-type": "application/json" },
+          })
+            .then((response) => response.text())
+            .then((text) => {
+              if (createKeyRef.current) createKeyRef.current.innerText = text;
+            });
+        }}
+      >
+        create key
+      </button>
+      <p ref={createKeyRef}></p>
       <button
         onClick={() => {
           socket.connect();
@@ -36,7 +61,9 @@ export default function App() {
       </button>
       <button
         onClick={() => {
-          socket.emit("event");
+          socket.emit("event", function (data: string) {
+            console.log(data);
+          });
         }}
       >
         event
