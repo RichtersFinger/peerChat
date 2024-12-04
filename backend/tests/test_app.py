6a74a440da8b694d681cfb8bb8513f93a55607e7
RichtersFinger <steffen.finger.privat@gmail.com>
2024-12-04 23:15:37 +0100

    [backend, tests] add test-data generation and MessageStore test

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -5,12 +5,14 @@ from pathlib import Path
 from shutil import rmtree
 from uuid import uuid4
 from json import dumps
+import random
+from datetime import datetime
 
 import pytest
 
 # pylint: disable=relative-beyond-top-level
 from ..app import load_config, load_auth, load_secret_key, app_factory
-from ..common import User, Auth
+from ..common import User, Auth, MessageStore
 
 
 @pytest.fixture(scope="session", name="tmp")
@@ -22,14 +24,64 @@ def _tmp():
 @pytest.fixture(scope="session", autouse=True)
 def fs_setup(request, tmp: Path):
     """Set up file_storage"""
-    tmp.mkdir(parents=True, exist_ok=True)
 
     def cleanup():
         """Cleanup tmp-dir."""
         if tmp.is_dir():
             rmtree(tmp)
+    cleanup()
 
+    tmp.mkdir(parents=True, exist_ok=True)
     request.addfinalizer(cleanup)
+
+
+def fake_conversation(dir_: Path) -> Path:
+    """
+    Creates a fake conversation and returns path to the disk-location.
+    """
+    id_ = str(uuid4())
+    length = random.randint(1, 5)
+    conversation_dir = dir_ / id_
+    conversation_dir.mkdir(parents=True, exist_ok=False)
+    index = conversation_dir / "index.json"
+    index.write_text(
+        dumps(
+            {
+                "id": id_,
+                "origin": ".".join(
+                    [str(random.randint(1, 255)) for _ in range(4)]
+                ),
+                "name": f"conversation {random.randint(1, 10)}",
+                "length": length,
+                "lastModified": datetime.now().isoformat(),
+            }
+        ),
+        encoding="utf-8",
+    )
+    for msg_id in range(length):
+        msg_file = conversation_dir / f"{msg_id}.json"
+        msg_file.write_text(
+            dumps(
+                {
+                    "id": msg_id,
+                    "body": random.choice(["cat", "dog", "bird"]),
+                    "status": random.choice(
+                        [
+                            "ok",
+                            "queued",
+                            "sending",
+                            "draft",
+                            "deleted",
+                            "error",
+                        ]
+                    ),
+                    "lastModified": datetime.now().isoformat(),
+                }
+            ),
+            encoding="utf-8",
+        )
+
+    return conversation_dir
 
 
 def unload_environment_variable(name: str):
@@ -166,3 +218,23 @@ def test_app_create_auth_key_user_value(request, tmp: Path):
     assert response.status_code == 200
     assert response.data == file.read_bytes()
     assert response.data == key.encode(encoding="utf-8")
+
+
+def test_message_store_loading_and_caching_conversation(tmp: Path):
+    """Test loading and caching of conversations in `MessageStore`."""
+    store = MessageStore(tmp)
+
+    # check behavior for missing data
+    assert store.load_conversation("unknown-id") is None
+
+    # prepare and load test-data
+    conversation_dir = fake_conversation(tmp)
+    conversation = store.load_conversation(conversation_dir.name)
+
+    assert conversation is not None
+    assert conversation.id_ == conversation_dir.name
+
+    # test caching
+    rmtree(conversation_dir)
+    conversation = store.load_conversation(conversation_dir.name)
+    assert conversation is not None
