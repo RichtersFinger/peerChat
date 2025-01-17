6e1ea2cce1557437eb545177328886945820b9f8
RichtersFinger <steffen.finger.privat@gmail.com>
2025-01-17 23:50:00 +0100

    [frontend, chat] add ChatBody component

--- frontend/src/components/Chat.tsx
+++ frontend/src/components/Chat.tsx
@@ -4,63 +4,22 @@ import { Avatar, Button, Textarea } from "flowbite-react";
 import { SocketContext } from "../App";
 import useConversation from "../hooks/useConversation";
 import ChatHeader from "./ChatHeader";
-import ChatMessageItem from "./ChatMessageItem";
+import ChatBody from "./ChatBody";
 
 export type ChatProps = {
   cid: string;
 };
 
-const DEFAULT_NMESSAGES = 10;
-const DEFAULT_NMESSAGES_INCREMENT = 10;
-
 export default function Chat({ cid }: ChatProps) {
   const socket = useContext(SocketContext);
   const conversation = useConversation(socket, cid);
-  const [nMessages, setNMessages] = useState<number>(DEFAULT_NMESSAGES);
   const newMessageRef = useRef<HTMLTextAreaElement>(null);
-
-  // reset initial values for states if cid changes
-  useEffect(() => {
-    setNMessages(DEFAULT_NMESSAGES);
-  }, [cid]);
 
   if (!socket) return null;
   return (
     <div className="flex flex-col w-full h-screen overflow-x-hidden">
       <ChatHeader conversation={conversation}/>
-      <div className="m-4 space-y-3 overflow-y-auto h-full">
-        <div className="justify-items-center">
-          {(conversation.length ?? 0) > 0 &&
-          nMessages < (conversation.length ?? 0) ? (
-            <div className="flex flex-row space-x-2">
-              <Button
-                onClick={() =>
-                  setNMessages((previous: number) =>
-                    Math.min(
-                      conversation.length ?? 0,
-                      previous + DEFAULT_NMESSAGES_INCREMENT
-                    )
-                  )
-                }
-              >
-                Load more
-              </Button>
-              <Button onClick={() => setNMessages(conversation.length ?? 0)}>
-                Load all
-              </Button>
-            </div>
-          ) : null}
-        </div>
-        <div className="space-y-3">
-          {Array(Math.min(conversation.length ?? 0, nMessages))
-            .fill(0)
-            .map((_, index) => (conversation.length ?? 0) - 1 - index)
-            .map((mid: number) => (
-              <ChatMessageItem key={mid} cid={cid} mid={mid.toString()} />
-            ))
-            .reverse()}
-        </div>
-      </div>
+      <ChatBody conversation={conversation}/>
       <div className="flex flex-row space-x-2 p-4">
         <Textarea ref={newMessageRef} placeholder="Your message..." rows={3} />
         <div>
---
+++ frontend/src/components/ChatBody.tsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import { Button } from "flowbite-react";
+
+import { Conversation } from "../hooks/useConversation";
+import ChatMessageItem from "./ChatMessageItem";
+
+export type ChatBodyProps = {
+  conversation: Conversation;
+};
+
+const DEFAULT_NMESSAGES = 10;
+const DEFAULT_NMESSAGES_INCREMENT = 10;
+
+export default function ChatBody({ conversation }: ChatBodyProps) {
+  const [nMessages, setNMessages] = useState<number>(DEFAULT_NMESSAGES);
+
+  // reset initial values for state if conversation changes
+  useEffect(() => {
+    setNMessages(DEFAULT_NMESSAGES);
+  }, [conversation]);
+
+  return (
+    <div className="m-4 space-y-3 overflow-y-auto h-full">
+      <div className="justify-items-center">
+        {(conversation.length ?? 0) > 0 &&
+        nMessages < (conversation.length ?? 0) ? (
+          <div className="flex flex-row space-x-2">
+            <Button
+              onClick={() =>
+                setNMessages((previous: number) =>
+                  Math.min(
+                    conversation.length ?? 0,
+                    previous + DEFAULT_NMESSAGES_INCREMENT
+                  )
+                )
+              }
+            >
+              Load more
+            </Button>
+            <Button onClick={() => setNMessages(conversation.length ?? 0)}>
+              Load all
+            </Button>
+          </div>
+        ) : null}
+      </div>
+      <div className="space-y-3">
+        {Array(Math.min(conversation.length ?? 0, nMessages))
+          .fill(0)
+          .map((_, index) => (conversation.length ?? 0) - 1 - index)
+          .map((mid: number) => (
+            <ChatMessageItem
+              key={mid}
+              cid={conversation.id}
+              mid={mid.toString()}
+            />
+          ))
+          .reverse()}
+      </div>
+    </div>
+  );
+}
