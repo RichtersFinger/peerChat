bd59bbec8979f2b74d84223f226fdea8802a9696
RichtersFinger <steffen.finger.privat@gmail.com>
2025-03-01 14:34:59 +0100

    [frontend, chat] add message-submission via 'Enter'

--- frontend/src/components/ChatInput.tsx
+++ frontend/src/components/ChatInput.tsx
@@ -11,27 +11,35 @@ export default function ChatInput({ cid }: ChatInputProps) {
   const socket = useContext(SocketContext);
   const newMessageRef = useRef<HTMLTextAreaElement>(null);
 
-  if (!socket) return null;
+  /**
+   * Attempts to send current input as message. Skip if socket is undefined.
+   */
+  function sendMessage() {
+    if (!socket) return;
+
+    socket.emit(
+      "post-message",
+      cid,
+      { body: newMessageRef.current?.value, isMine: true },
+      (mid: number) => {
+        if (newMessageRef.current?.value) newMessageRef.current.value = "";
+        socket.emit("send-message", cid, mid);
+      }
+    );
+  }
+
   return (
     <div className="flex flex-row space-x-2 p-4">
-      <Textarea ref={newMessageRef} placeholder="Your message..." rows={3} />
+      <Textarea
+        ref={newMessageRef}
+        placeholder="Your message..."
+        rows={3}
+        onKeyDown={(e) => {
+          if (!e.shiftKey && e.key === "Enter") sendMessage();
+        }}
+      />
       <div>
-        <Button
-          onClick={() => {
-            socket.emit(
-              "post-message",
-              cid,
-              { body: newMessageRef.current?.value, isMine: true },
-              (mid: number) => {
-                if (newMessageRef.current?.value)
-                  newMessageRef.current.value = "";
-                socket.emit("send-message", cid, mid);
-              }
-            );
-          }}
-        >
-          Send
-        </Button>
+        <Button onClick={() => sendMessage()}>Send</Button>
       </div>
     </div>
   );
